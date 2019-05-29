63376ddc404f4e824d98165a18d5114b453438f0
StephanieeV <44191457+StephanieeV@users.noreply.github.com>
2019-05-29 14:06:28 +0200

    Update serveur.php

--- Fichiers php/serveur.php
+++ Fichiers php/serveur.php
@@ -9,64 +9,71 @@ if ($_REQUEST["operation"]=="enreg"){
     $auth = json_decode($data) ;
     $login = $auth[0] ;
     $mdp = $auth[1] ;
-	
+
     try {
         $leVisiteur = $pdo->getInfosVisiteur($login,$mdp);
-        
+
         if($leVisiteur){
-            print_r("ok");
+            print_r("Connexion-OK%");
             $idVisiteur = $leVisiteur[0];
-            
+
             $donnee = $_REQUEST["lesdonnees"];
             $lesMois = json_decode($donnee);
-            
+
             foreach ($lesMois as $unMois){
                 $leMois= $unMois[0];
-                
+
                 if (!$pdo->existeFicheFrais($idVisiteur, $leMois)){
                     $pdo->creeNouvellesLignesFrais($idVisiteur, $leMois);
-                    print ("creation fiche");
+                    print ("Création nouvelle fiche");
+                } else {
+                    print"Fiche déjà existante";
+                    var_dump($pdo->existeFicheFrais($idVisiteur, $leMois));
                 }
+
                 $data = $_REQUEST["lesdonnees"];
+
                 $lesDonnees = json_decode($data);
+
                 foreach ($lesDonnees as $uneDonnee){
+
                     $lesFraisForfait = array(
                         "ETP" => $uneDonnee[1],
-			"KM" => $uneDonnee[2],
-			"NUI" => $uneDonnee[3],
-			"REP" => $uneDonnee[4]);
-                    
+                        "KM" => $uneDonnee[2],
+                        "NUI" => $uneDonnee[3],
+                        "REP" => $uneDonnee[4]);
+
                     $pdo->majFraisForfait($idVisiteur, $leMois, $lesFraisForfait);
-	            print("enregFraisForfait-OK%");
-                    
+                    print("enregFraisForfait-OK%");
+
                     $lesFraisHf = $uneDonnee[5];
-                    foreach ($lesFraisHf as $unFraisHf){
+
+                    foreach($lesFraisHf as $key => $value){
+
                         $annee = substr($leMois, 0, 4);
-			$mois = substr($leMois, 4, 2);
-			$jour = $unFraisHf[0];
-			if (strlen($jour) == 1){
-				$jour = "0" . $jour;
-			}
-			$date = $jour . '/' . $mois . '/' . $annee;
-			$montant = $unFraisHf[1];
-			$libelle = $unFraisHf[2];
-					
-			// on vérifie si le frais hors forfait n'est pas déjà enregistré
-			if (!$pdo->existeFraisHorsForfait($idVisiteur, $leMois, $libelle, $date, $montant)){
-				// alors on l'enregistre
-				$pdo->creeNouveauFraisHorsForfait($idVisiteur, $leMois, $libelle, $date, $montant);
-				print("enregFraisHF-OK%");
-			}
+                        $mois = substr($leMois, 4, 2);
+                        $jour = $value[0];
+
+                        if (strlen($jour) == 1){
+                            $jour = "0" . $jour;
+                        }
+
+                        $date = $jour . '/' . $mois . '/' . $annee;
+                        $montant = $value[1];
+                        $libelle = $value[2];
+
+                        $pdo->creeNouveauFraisHorsForfait($idVisiteur, $leMois, $libelle, $date, $montant);
+                        print("enregFraisHF-OK%");
+                        
                     }
                 }
             }
         } else {
-            print ("pas co");
+            print ("Identifiants incorrectes");
         }
     } catch (Exception $e) {
         print("erreur");
         die();
-
     }
 }
-?>
+?>					
